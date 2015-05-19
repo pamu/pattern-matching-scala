7c2a2e55dd76250c04afa81c5b634361d582d772
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-05-20 01:24:41 +0530

    apply, unapply code

--- src/main/scala/com/nagarjuna_pamu/Ex1.scala
+++ src/main/scala/com/nagarjuna_pamu/Ex1.scala
@@ -1,11 +1,18 @@
 package com.nagarjuna_pamu
 
-object Ex1 {
+class Fraction(val num: Int, val denum: Int) {
+  def *(that: Fraction) = Fraction(this.num * that.num, this.denum * that.denum)
+  override def toString = s"$num/$denum"
+}
 
+object Fraction {
+  def apply(num: Int, denum: Int) = new Fraction(num, denum)
+  def unapply(input: Fraction) = if (input.denum == 0) None else Some((input.num, input.denum))
 }
 
 object DemoIt {
   def main(args: Array[String]): Unit = {
-    
+    var Fraction(num, denum) = Fraction(1, 2) * Fraction(1, 4)
+    println(num + " " + denum)
   }
 }
